be78fd9693bb5d1e5ac8ed8040165bdda1d6eeeb
EC2 Default User <ec2-user@ip-172-31-62-43.ec2.internal>
2021-09-07 02:56:39 +0000

    follow & unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :users, only: [:index, :show, :edit, :update]
-  get '/home/about' => 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  
+  get '/home/about' => 'homes#about'
 
 end
