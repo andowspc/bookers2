32e407399285ac0435f8e72cdc20ed2d708446b8
EC2 Default User <ec2-user@ip-172-31-62-43.ec2.internal>
2021-09-09 08:06:34 +0000

    Javascript

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,11 +5,16 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new (book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  render 'books/show'
-		end
+		unless @book_comment.save
+      render 'error' 
+    end
+    # app/views/book_comments/create.js.erbを参照する
+		
+		# if @book_comment.save
+  		# redirect_to book_path(@book.id)
+		# else
+		  # render 'books/show'
+		# end
   end
   
   # show/create計2回でnewを置き、
@@ -19,7 +24,7 @@ class BookCommentsController < ApplicationController
   	@book = Book.find(params[:book_id])
   	book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to book_path(@book.id)
+		# redirect_to book_path(@book.id)
 		# =redirect_to request.referer(遷移元のURLにリダイレクト)
   end
 
